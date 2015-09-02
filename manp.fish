1c8964872a502fae35601193ec4e2673954e4da2
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-09-01 22:56:05 -0300

    Show a message if no argument is given to `manp`

--- manp.fish
+++ manp.fish
@@ -1,3 +1,7 @@
 function manp -d "Open a specified man page in Preview"
-  man -t $argv | open -f -a Preview
+  if [ (count $argv) -gt 0 ]
+    man -t $argv | open -f -a Preview
+  else
+    echo "No arguments given"
+  end
 end
