96d2c617e5d336f0d5ca470685c1937b1bf30998
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-09-01 22:48:08 -0300

    Fix `ql` function in order to it actually work
    It just ignored any function argument and just quick looked into
    Applications/ :confused:

--- ql.fish
+++ ql.fish
@@ -1,6 +1,6 @@
-function ql -d "Quick Look a specified file"
+function ql -d "Quick Look a specified file or directory"
   if [ (count $argv) -gt 0 ]
-    qlmanage >/dev/null ^/dev/null -p Applications/ &
+    qlmanage >/dev/null ^/dev/null -p $argv &
   else
     echo "No file or folder as argument given"
   end
