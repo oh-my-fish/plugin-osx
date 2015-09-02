270b7b84808b1049c5c0ab8aad61dbfaeff0e8b7
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-09-01 22:08:20 -0300

    Cleanup trailing spaces and adding newlines at EOF

--- itunes.fish
+++ itunes.fish
@@ -4,15 +4,15 @@ function itunes -d "Play, pause etc. iTunes. Use -h or --help for a more detaile
   if [ (count $argv) -gt 0 ]
     set -l opt $argv[1]
     switch $opt
-      case launch play pause stop rewind resume quit      
+      case launch play pause stop rewind resume quit
       case mute
-        set opt "set mute to true"      
+        set opt "set mute to true"
       case unmute
-        set opt "set mute to false"      
+        set opt "set mute to false"
       case next previous
-        set opt "$opt track"      
+        set opt "$opt track"
       case vol volume
-        set opt "set sound volume to $argv[2]"      
+        set opt "set sound volume to $argv[2]"
       case "" -h --help
         echo "Usage: itunes <option>"
         echo "option:"
@@ -21,10 +21,10 @@ function itunes -d "Play, pause etc. iTunes. Use -h or --help for a more detaile
         echo \t"next|previous\tplay next or previous track"
         echo \t"vol"\t"Set the volume, takes an argument from 0 to 100"
         echo \t"help"\t"show this message and exit"
-        return 0      
+        return 0
       case '*'
         echo "Unknown option $opt. Use -h or --help for a more detailed description."
-        return 1      
+        return 1
     end
     osascript -e "tell application \"iTunes\" to $opt"
   else
