0408d3db1df4755a9aed043181f2f8126d4f627f
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-09-01 22:47:07 -0300

    Fix indentation of `pfd` and `pfs`

--- pfd.fish
+++ pfd.fish
@@ -2,7 +2,7 @@
 
 function pfd -d "Return the path of the frontmost Finder window"
   osascript 2>/dev/null -e '
-      tell application "Finder"
-        return POSIX path of (target of window 1 as alias)
-      end tell'
+    tell application "Finder"
+      return POSIX path of (target of window 1 as alias)
+    end tell'
 end
